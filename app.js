2bf9e4f50a62d2821bd676a093cd1531b91fac07
Mengye Wei <mengyewei@gmail.com>
2020-12-18 20:10:39 -0800

    index update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const mobile_item = document.querySelectorAll('.header .nav-bar .nav-list ul a');
-const header = document.querySelector('.header .container');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul a');
+const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
@@ -11,7 +11,7 @@ hamburger.addEventListener('click',()=>{
 document.addEventListener('scroll',()=>{
     var scroll_position = window.scrollY;
     if(scroll_position > 200){
-        header.style.backgroundColor = "black";
+        header.style.backgroundColor = "#708090";
     }else{
         header.style.backgroundColor = "transparent";
     }
